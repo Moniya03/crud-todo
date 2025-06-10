bec4d0969fd1a5d6e9fac6fae31163ba41b2b178
Moniya03 <moniyadhingra1703@gmail.com>
2025-06-10 17:24:56 +0530

    connect db

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import connectDB from "./db/connectDB.js";
+import dotenv from "dotenv";
+import { app } from "./app.js";
+
+const PORT = process.env.PORT || 8000;
+
+dotenv.config({
+  path: "./.env",
+});
+
+connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.log("ERR: ", error);
+      throw error;
+    });
+    app.listen(PORT, () => {
+      console.log(`Server is running at ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MongoDB Connection Failed - ", err);
+  });
